4b136b8434487c58a9bfa813f290de99e27a055d
vexGuide <54220575+vexGuide@users.noreply.github.com>
2019-08-18 22:02:54 -0400

    Update canDrive.cpp

--- examples/canDrive.cpp
+++ examples/canDrive.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+
 int main(void) {
     int age = 21; //declare “age” and set it to 21
     bool canDrive; //make a bool to make sure they can drive
@@ -5,9 +7,11 @@ int main(void) {
 	
     if(age >= 16) {
         canDrive = true;
+        std::cout << "You can drive!"; //std::cout prints the words in quotes to the screen
     } 
     else {
         canDrive = false;
+        std::cout << "You cannot drive.";
     }
 	
     return 0;
