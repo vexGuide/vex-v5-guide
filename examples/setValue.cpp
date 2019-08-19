ace6be319cf0f173a75cffae3a0e88b0dad69776
vexGuide <54220575+vexGuide@users.noreply.github.com>
2019-08-18 20:58:20 -0400

    Create setValue.cpp

---
+++ examples/setValue.cpp
@@ -0,0 +1,9 @@
+int setValue(int var) { //define a function with return type “int”
+    return var; //returns an integer (var)
+    //end function
+}
+
+int main(void) {
+    int a = setValue(5); //calls setValue, gives it 5, and the function returns 5, setting a to 5
+    return 0; //end program
+}
