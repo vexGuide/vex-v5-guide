b8e85e1bf93b1bde56b3b119600c52920a0f8470
vexGuide <54220575+vexGuide@users.noreply.github.com>
2019-08-18 21:45:53 -0400

    Update canDrive.cpp

--- examples/canDrive.cpp
+++ examples/canDrive.cpp
@@ -1,6 +1,6 @@
 int main(void) {
-	int age = 21; //declare “age” and set it to 21
-	bool canDrive; //make a bool to make sure they can drive
+    int age = 21; //declare “age” and set it to 21
+    bool canDrive; //make a bool to make sure they can drive
     //notice how it is not initialized because we don’t need to yet
 	
     if(age >= 16) {
