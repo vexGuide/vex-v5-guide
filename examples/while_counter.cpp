24e492f40a2513d184a00f6a0cd0f8ae58429f6e
Evan Hess <enh.robotics@gmail.com>
2019-08-19 21:30:16 -0400

    Update while_counter.cpp

--- examples/while_counter.cpp
+++ examples/while_counter.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-int main(void)
-{
+int main(void) {
 	int a = 1; //set a to 1
 	while(a < 10) { //if a is less than 10, keep going
-		a = a + 1;
+		std::cout << a << endl; //print the value
+		a++; //increment by 1
 	}
 	std::cout << a;
 	return 0;
